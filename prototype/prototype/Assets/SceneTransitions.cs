b910b96bb402d802a8e304313a5aefda43cc18c4
SugawaraRin <ringawara@gmail.com>
2022-05-08 23:36:09 +0900

    フィールド塗り替え

---
+++ prototype/prototype/Assets/SceneTransitions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitions : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("Button_B"))
+        {
+            SceneManager.LoadScene("TitleScene", LoadSceneMode.Single);
+        }
+    }
+}
---
+++ prototype/prototype/Assets/Scenes/Script/title.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class title : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("Button_B"))
+        {
+            SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+        }
+    }
+}
